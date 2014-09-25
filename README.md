cd40997ec1e4e37f3c9690761529d7cec30a1e1d
Alexander Graham <nagraham00@hotmail.com>
2014-09-25 13:46:02 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ruby on Rails Tutorial: Chapter 2 Demo App
+
+This is the second application in the [*Ruby on Rails Tutorial*](http://railstutorial.org/) by [Michael Hartl](http://michaelhartl.com/). nagraham is developing this to get a fuller understanding of rails.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '6deefa69bcba445a0d7221036a07cbf188349013b0de0bad47648e6b72496e8a87f4c6a980b0cc3bbac4ff10646ae1c6e4bcfde806a54517548c091a8520e26b'
